cdec9df813259cf6c1f004eaefe8b7893a33bb7f
Chamodh Dasanayake <78280086+Cham-88D@users.noreply.github.com>
2022-05-07 13:58:10 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeCarousel from "./components/pages/HomePage/Carousel/Carousel";
-
+import  Nav from "./components/nav_bar/nav"
 function App() {
   return (
     <div >
-      <HomeCarousel/>
+      <Nav/>
     </div>
   );
 }
