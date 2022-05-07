28eac6d19cec391a15b13aaacbb116eb1c4e0385
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-07 17:18:52 +0530

    movie grid created

---
+++ src/components/pages/HomePage/Movies/MovieGrid.jsx
@@ -0,0 +1,23 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import { Container } from "react-bootstrap";
+import MovieCard from "./MovieCard";
+import Movies from "./movies.json";
+
+const MovieGrid = () => {
+    const movies = Movies.data;
+
+    return (
+        <>
+            <Container className="movie-grid-wrapper">
+                <Grid container spacing={3} className="movie-grid-flex">
+                    {movies.map((movie) => (
+                        <MovieCard key={movie.id} movieDetails={movie}/>
+                    ))}
+                </Grid>
+            </Container>
+        </>
+    )
+}
+
+export default MovieGrid;
