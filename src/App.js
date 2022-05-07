6d49278495792c59e7c75683fe55502637876520
yasith <it19966922@my.sliit.lk>
2022-05-07 21:59:36 +0530

    create movie list page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'antd/dist/antd.css';
 import HomeCarousel from "./components/pages/HomePage/Carousel/Carousel";
+import MovieList from "./components/admin pages/Movies/MovieList";
 
 function App() {
   return (
     <div >
-      <HomeCarousel/>
+      <MovieList/>
     </div>
   );
 }
