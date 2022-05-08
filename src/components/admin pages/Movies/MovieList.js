e86d27f2c0dda7ebe5f7a661d4f46c07312ffcdb
yasith <it19966922@my.sliit.lk>
2022-05-08 11:32:03 +0530

    create edit movie modal

--- src/components/admin pages/Movies/MovieList.js
+++ src/components/admin pages/Movies/MovieList.js
@@ -8,11 +8,16 @@ import withReactContent from 'sweetalert2-react-content'
 
 const MovieList = () => {
     const MySwal = withReactContent(Swal)
-    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isAddMovieModalVisible, setIsAddMovieModalVisible] = useState(false);
+    const [isEditMovieModalVisible, setIsEditMovieModalVisible] = useState(false);
     const [loading, setLoading] = useState(true);
 
-    const showModal = () => {
-        setIsModalVisible(true);
+    const showAddMovieModal = () => {
+        setIsAddMovieModalVisible(true);
+    };
+
+    const showEditMovieModal = () => {
+        setIsEditMovieModalVisible(true);
     };
 
     const handleOk = async () => {
@@ -21,11 +26,15 @@ const MovieList = () => {
             html: <i>You clicked the button!</i>,
             icon: 'success'
         })
-        setIsModalVisible(false);
+        setIsAddMovieModalVisible(false);
     };
 
     const handleCancel = () => {
-        setIsModalVisible(false);
+        setIsAddMovieModalVisible(false);
+    };
+
+    const handleEditMovieModalCancel = () => {
+        setIsEditMovieModalVisible(false);
     };
 
     const onFinish = (values) => {
@@ -60,7 +69,7 @@ const MovieList = () => {
                                         <input type="text" className="form-control" placeholder="Search"/>
                                         <button className="btn btn-default mx-3" title="Reload"><i
                                             className="fa-solid fa-magnifying-glass"></i></button>
-                                        <button className="btn btn-default" title="Add new" onClick={showModal}><i
+                                        <button className="btn btn-default" title="Add new" onClick={showAddMovieModal}><i
                                             className="fa-solid fa-plus"></i> Add Movie</button>
                                     </div>
                                 </div>
@@ -89,7 +98,7 @@ const MovieList = () => {
                                         <td>cast</td>
                                         <td>
                                             <ul className="action-list">
-                                                <li><a href="#" data-tip="edit"><i className="fa fa-edit"></i></a></li>
+                                                <li><a href="#" data-tip="edit" onClick={showEditMovieModal}><i className="fa fa-edit"></i></a></li>
                                                 <li><a href="#" data-tip="delete"><i className="fa fa-trash"></i></a></li>
                                             </ul>
                                         </td>
@@ -101,7 +110,7 @@ const MovieList = () => {
                 </div>
             </div>
 
-            <Modal title="Add Movie" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <Modal title="Add Movie" visible={isAddMovieModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
                 <Form
                     name="basic"
                     initialValues={{ remember: true }}
@@ -164,7 +173,77 @@ const MovieList = () => {
                     </Form.Item>
 
                     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit" loading>
+                        <Button type="primary" htmlType="submit" loading shape="round" size="large">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
+
+            <Modal title="Edit Movie" visible={isEditMovieModalVisible} onOk={handleOk} onCancel={handleEditMovieModalCancel} footer={null}>
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    labelCol={{ span: 6 }}
+                    wrapperCol={{ span: 16 }}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Movie Name"
+                        name="name"
+                        tooltip="This is a required field"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Cast"
+                        name="cast"
+                        tooltip="This is a required field"
+                        rules={[{ required: true, message: 'Please input movie cast!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Theater"
+                        name="theater"
+                        tooltip="This is a required field"
+                        rules={[{ required: true, message: 'Please select a theater!' }]}
+                    >
+                        <Select defaultValue="lucy"  onChange={handleChange}>
+                            <Option value="jack">Jack</Option>
+                            <Option value="lucy">Lucy</Option>
+                            <Option value="disabled" disabled>
+                                Disabled
+                            </Option>
+                            <Option value="Yiminghe">yiminghe</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Time"
+                        name="time"
+                        tooltip="This is a required field"
+                        rules={[{ required: true, message: 'Please input time!' }]}
+                    >
+                        <TimePicker.RangePicker onChange={onTimeChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Ticket Price"
+                        name="price"
+                        tooltip="This is a required field"
+                        rules={[{ required: true, message: 'Please input ticket price!' }]}
+                    >
+                        <InputNumber prefix="$" style={{ width: '100%' }} />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit" loading shape="round" size="large">
                             Submit
                         </Button>
                     </Form.Item>
