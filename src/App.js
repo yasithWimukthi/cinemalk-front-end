24ed4a41569f1cf5441dd77b432c76c396ee7ead
yasith <it19966922@my.sliit.lk>
2022-05-08 22:24:55 +0530

    create theater component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeCarousel from "./components/pages/HomePage/Carousel/Carousel";
 import  Nav from "./components/nav_bar/nav"
 import MovieList from "./components/admin pages/Movies/MovieList";
+import TheaterDetails from "./components/admin pages/TheaterDetails/TheaterDetails";
 function App() {
   return (
     <div >
-      <MovieList/>
+      <TheaterDetails/>
     </div>
   );
 }
