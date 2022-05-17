5390f6cf5513e03cffdcb5964b2f2c0551e20e1c
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-17 15:05:20 +0530

    fixed errors in axios request calls

--- src/API/util.js
+++ src/API/util.js
@@ -22,16 +22,12 @@ export const getRequest = (uri) => {
 export const getByIDRequest = (uri, id) => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`${BASE_URL}${uri}`, {
-        params: {
-          ID: id,
-        },
-      })
+      .get(`${BASE_URL}${uri}${id}`)
       .then(function (response) {
         resolve(response);
       })
       .catch(function (error) {
-        console.log(error);
+        console.log('errrrr', error);
         reject(error);
       })
       .then(function () {
@@ -44,8 +40,9 @@ export const postRequest = (uri, data) => {
   //data is an object
   return new Promise((resolve, reject) => {
     axios
-      .post(`${BASE_URL}${uri}`, { data })
+      .post(`${BASE_URL}${uri}`, data)
       .then(function (response) {
+        console.log('add result', response)
         resolve(response);
       })
       .catch(function (error) {
@@ -58,17 +55,13 @@ export const postRequest = (uri, data) => {
 export const updateRequest = (uri, id, data) => {
   return new Promise((resolve, reject) => {
     axios
-      .put(`${BASE_URL}${uri}`, {
-        data, 
-        params: {
-          ID: id
-        }
-      })
+      .put(`${BASE_URL}${uri}${id}`, data)
       .then(function (response) {
+        console.log('success', response);
         resolve(response);
       })
       .catch(function (error) {
-        console.log(error);
+        console.log('errrrrr', error);
         reject(error);
     })
   })
@@ -77,12 +70,9 @@ export const updateRequest = (uri, id, data) => {
 export const deleteRequest = (uri, id) => {
   return new Promise((resolve, reject) => {
     axios
-      .delete(`${BASE_URL}${uri}`, {
-        params: {
-          ID: id,
-        },
-      })
+      .delete(`${BASE_URL}${uri}${id}`)
       .then(function (response) {
+        console.log('deleted', response);
         resolve(response);
       })
       .catch(function (error) {
