358e2abcb5d4547c83354758c19de98078eb80e0
yasith <it19966922@my.sliit.lk>
2022-05-19 19:43:15 +0530

    add comments

--- src/API/Admin pages/TheatersAPI.js
+++ src/API/Admin pages/TheatersAPI.js
@@ -2,12 +2,42 @@ import { getByIDRequest, getRequest, postRequest, updateRequest, deleteRequest }
 
 const BASE_URL = "http://localhost:4000";
 
+/**
+ * @description - Get all the theaters
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 export const getTheaters = (uri) => getRequest(BASE_URL, uri);
 
+/**
+ * @description - Get a theater by id
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 export const getTheaterById = (uri, id) => getByIDRequest(BASE_URL, uri, id);
 
+/**
+ * @description - Create a new theater
+ * @param uri
+ * @param data
+ * @returns {Promise | Promise<unknown>}
+ */
 export const addTheater = (uri, data) => postRequest(BASE_URL, uri, data);
 
+/**
+ * @description - Update a theater
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 export const updateTheater = (uri, id, data) => updateRequest(BASE_URL, uri, id, data);
 
+/**
+ * @description - Delete a theater
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
 export const deleteTheater = (uri, id) => deleteRequest(BASE_URL, uri, id);
