7e19f65661c735045478263e8cd94af6d285c120
yasith <it19966922@my.sliit.lk>
2022-05-19 19:46:56 +0530

    add comments

--- src/API/Auth/auth.js
+++ src/API/Auth/auth.js
@@ -1,12 +1,18 @@
 import http from "./base";
 
-
-
-
+/**
+ * @description: signup
+ * @param {type}
+ * @return:
+ */
 const SignUp = (data)=>{
     return http.post("/signup",data);
 }
 
+/*
+ * @description: login
+ * @param {type}
+ */
 const SignIn = (data)=>{
     return http.post("/login",data)
 }
