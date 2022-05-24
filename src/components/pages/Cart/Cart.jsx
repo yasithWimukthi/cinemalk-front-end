b0903706f29c3d3ec7d6bd1a68b0ae4693e92a4f
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-24 19:40:49 +0530

    fetch cart details

--- src/components/pages/Cart/Cart.jsx
+++ src/components/pages/Cart/Cart.jsx
@@ -1,13 +1,42 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import CartItem from "./CartItem";
 import Swal from 'sweetalert2'
 import "./cart.scss";
+import { getCartByUserId } from "../../../API/Customer pages/CartAPI";
 
 
 const Cart = () => {
+  let totalNoOfBookings = 0;
   const [isPayFromCard, setIsPayFromCard] = useState(false);
   const [isPayFromMobile, setIsPayFromMobile] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartDetails, setCartDetails] = useState({});
+  const [totalNoOfTickets, setTotalNoOfTickets] = useState(0);
+
+  const uid = localStorage.getItem('user_id');
+
+      // fetch theater details from backend theater service
+      const getcartItems = () => {
+        getCartByUserId("/api/cart/", uid)
+          .then((res) => {
+            setCartItems(res.data.cart.items);
+            console.log('cart', res.data.cart);
+            setCartDetails(res.data.cart)
+            res.data.cart.items.map((item) => (
+              totalNoOfBookings += item.noOfTickets
+            ))
+            setTotalNoOfTickets(totalNoOfBookings)
+        
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    useEffect(() => {
+      getcartItems(); //details of all theaters will be fetched when component renders for the first time
+    }, []);
 
   const showPayFromCard = () => {
     setIsPayFromCard(true);
@@ -47,29 +76,30 @@ const Cart = () => {
 
   return (
     <>
-
       <Container className="cart-page-wrapper">
         <h2></h2>
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {
+          cartItems.map((item) => (
+            <CartItem key={item._id} bookingDetails={item} />
+          ))
+        }
         {/* cart total */}
         <div className="total-price-card-wrapper">
           <Row>
             <Col sm={3} className="total-left-wrap">
             </Col >
             <Col sm={9} className="total-right-wrap">
-            <div className="total-right-card1">
+              {/* <div className="total-right-card1">
                 <h6 >Ticket price: </h6>
                 <h6 >RS. 500.00</h6>
-              </div>
-            <div className="total-right-card1">
-                <h6 >No of bookings: </h6>
-                <h6 >3</h6>
+              </div> */}
+              <div className="total-right-card1">
+                <h6 >Total no of bookings: </h6>
+                <h6 >{totalNoOfTickets}</h6>
               </div>
               <div className="total-right-card">
                 <h6 >Total price: </h6>
-                <h6 >Rs. 1500.00</h6>
+                <h6 >{"RS. "+cartDetails.totalPrice+".00"}</h6>
               </div>
               <hr className="straight-line" />
               <div className="checkout-btn">
