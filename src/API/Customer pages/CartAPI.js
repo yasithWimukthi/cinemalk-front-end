c1d916be84272c855883f04ba81a963b1cf271d4
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-25 16:16:23 +0530

    refactored

--- src/API/Customer pages/CartAPI.js
+++ src/API/Customer pages/CartAPI.js
@@ -2,12 +2,8 @@ import { getByIDRequest, getRequest, postRequest, updateRequest, deleteRequest }
 
 const BASE_URL = "http://localhost:4001";
 
-export const getTheaters = (uri) => getRequest(BASE_URL, uri);
-
 export const getCartByUserId = (uri, id) => getByIDRequest(BASE_URL, uri, id);
 
 export const addToCart = (uri, data) => postRequest(BASE_URL, uri, data);
-
-export const updateTheater = (uri, id, data) => updateRequest(BASE_URL, uri, id, data);
 
 export const removeFromCart = (uri, id) => deleteRequest(BASE_URL, uri, id);
--- src/API/Reservation pages/ReservationCancelAPI.js
+++ src/API/Reservation pages/ReservationCancelAPI.js
@@ -1,28 +1,8 @@
-// import { getRequest, deleteRequest } from "../util";
-
-// const BASE_URL = "http://localhost:4000";
-
-// /**
-//  * @description - Get all reservations
-//  * @param {string} id - Reservation id
-//  * @returns {object} - Reservation object
-//  */
-// export const getReservations = (uri) => getRequest(BASE_URL, uri);
-
-// /**
-//  * @description cancel reservation
-//  * @param {string} reservationId
-//  * @returns {object} reservation
-//  */
-// export const deleteReservation = (uri, id) => deleteRequest(BASE_URL, uri, id);
-
-import { getByIDRequest, getRequest, postRequest, updateRequest, deleteRequest } from "../util";
+import { getByIDRequest, deleteRequest } from "../util";
 
 const BASE_URL = "http://localhost:4001";
 
 export const getReservationsByUserId = (uri, id) => getByIDRequest(BASE_URL, uri, id);
-
-export const addToCart = (uri, data) => postRequest(BASE_URL, uri, data);
 
 export const deleteReservation = (uri, id) => deleteRequest(BASE_URL, uri, id);
 
